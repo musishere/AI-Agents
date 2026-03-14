2d66440ae1ad47963579828d14c9a1604747bf34
musishere <mustafabukhari333@gmail.com>
2026-03-14 23:25:31 +0500

    feat: add Express server with /ask endpoint for structured queries

---
+++ 02-Json-Structured-Output/backend/src/server.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import { askStructured } from "./ask-core";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET", "OPTIONS", "DELETE"],
+    allowedHeaders: ["Content-type", "Authorization"],
+    credentials: false,
+  }),
+);
+
+app.use(express.json());
+
+app.post("/ask", async (req, res) => {
+  try {
+    const { query } = req.body;
+    if (!query) {
+      return res.status(400).json({ error: "query required" });
+    }
+
+    const out = await askStructured(query);
+
+    return res.status(200).json(out);
+  } catch (error) {
+    return res.status(500).json({ message: "Intervel server error", error });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Api is listening to port ${PORT}`);
+});
